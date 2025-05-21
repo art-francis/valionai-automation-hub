24da381d74e82b60efa83a2a0f03f4c4bb61c962
Francis <artfrancis08@gmail.com>
2025-05-21 14:22:45 +0900

    updated footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -46,7 +46,7 @@ const Footer = () => {
             <h3 className="text-lg font-semibold text-white mb-4">Company</h3>
             <ul className="space-y-2">
               <li><Link to="/about" className="text-gray-400 hover:text-white transition-colors">About Us</Link></li>
-              <li><Link to="/blog" className="text-gray-400 hover:text-white transition-colors">Blog</Link></li>
+              <li><Link to="https://blog.valionai.com/" className="text-gray-400 hover:text-white transition-colors">Blog</Link></li>
               <li><Link to="/contact" className="text-gray-400 hover:text-white transition-colors">Contact</Link></li>
             </ul>
           </div>
